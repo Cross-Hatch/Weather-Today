6acf35856988484bd5edb4164baa2eb757b3d1c2
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-04-29 23:53:32 +0000

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './components/GlobalStyle.style';
+import Header from './components/Header/Header';
+import { theme } from './components/ThemeProvider.style';
+
 function App() {
-  return null
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Header />
+    </ThemeProvider>
+  );
 }
 
 export default App;
