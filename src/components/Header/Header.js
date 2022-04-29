cb107db27bf476617d7ad99c2bc9173546b7a7fd
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-04-29 23:51:27 +0000

    Added Hearder.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import Humburger from '../Humburger/Humburger';
+import { Container } from './Header.style';
+
+const Header = () => {
+  return (
+    <Container>
+      <Humburger />
+    </Container>
+  );
+};
+
+export default Header;
