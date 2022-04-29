bad950497c3ec9415e3d1770922d9587e09c44a5
David <davebostons2017@gmail.com>
2022-04-29 11:51:08 +0000

    imported react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
+import React from "react";
+import axios from "axios";
+
+
 function App() {
+  const [data,setData] = useState({})
+  const [location, setLocation] = useState('')
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=68907bf609585ee2ef82afc4cdc53829`
+  const searchLocation = (event) =>{
+    if (event.key ==='Enter'){
+      axios.get(url).then((response)=>{
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    } 
+  }
   return null
 }
 
