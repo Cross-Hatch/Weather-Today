bc3b96947c22903554f37e220756b8b47b666df2
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-04-30 02:17:40 +0000

    Added City.style.js

---
+++ src/components/City/City.style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+export const CityName = styled.h2`
+  font-size: 2.2rem;
+`;
+
+export const TimeZone = styled.h3`
+  font-size: 1rem;
+  font-weight: 700;
+  letter-spacing: 0.15em;
+`;
