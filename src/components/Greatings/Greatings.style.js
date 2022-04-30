5a06c00b16cffc36e58d660cba9f2a5cc37ad650
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-04-30 15:41:57 +0000

    Added Greatings.style.js

---
+++ src/components/Greatings/Greatings.style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 2em;
+`;
+
+export const Greating = styled.div`
+  font-size: 1.5rem;
+`;
+
+export const Day = styled.div`
+  font-size: 4rem;
+  font-weight: 700;
+  line-height: 0.8;
+`;
