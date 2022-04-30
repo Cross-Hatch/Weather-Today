0098a33501d7e48bf809cbf91ad183a65c320a16
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-04-30 15:37:41 +0000

    Modified SearchBar.style.js
    - Changed container bottom to 0
    - Reduced box width to 17em
    - Added placeholder styles

--- src/components/SearchBar/SearchBar.style.js
+++ src/components/SearchBar/SearchBar.style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   position: absolute;
-  bottom: 5vh;
+  bottom: 0vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -12,13 +12,17 @@ export const Container = styled.div`
 
 export const SearchBox = styled.input`
   background-color: rgba(124, 124, 124, 0.2);
-  width: 20em;
+  width: 17em;
   height: 3.5em;
   border: none;
   border-radius: 1.75em;
   outline: none;
   margin: 0 0.5em 0 0.5em;
   padding: 1em 1.2em;
+
+  &::placeholder {
+    font-weight: 700;
+  }
 `;
 
 export const SearchButton = styled.div`
