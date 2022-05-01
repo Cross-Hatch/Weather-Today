1a9623befc830455e1d2af420fdbbe1e88a7d7d6
David <davebostons2017@gmail.com>
2022-05-01 14:55:15 +0000

    added city_name,temp, humidity, wind speed and feels divs

--- src/App.js
+++ src/App.js
@@ -15,7 +15,42 @@ function App() {
       setLocation('')
     } 
   }
-  return null
+  return (
+  <div className="app">
+  <div className="search">
+    <input value ={location}
+           onChange={event => setLocation(event.target.value)}
+           onKeyPress={searchLocation}
+           placeholder='Enter Location'
+           type="text"
+    />
+  </div>
+  <div className="container">
+    <div className="top">
+    <div className="location"><p>{data.name}</p></div>
+    <div className="temp">{data.main ?<h1>{data.main.temp}°C</h1>:null}</div>
+    <div className="description">{data.weather?<p>{data.weather[0].main}</p>:null}</div>
+    </div>      
+    
+    <div className="bottom">
+    <div className="feels">
+    <p>Feels like</p>
+      {data.main?<p className="bold">{data.main.feels_like}°C</p>:null}
+    
+    </div>
+    <div className="humidity">
+      <p>Humidity</p>
+      {data.main ?<p className="bold">{data.main.humidity}%</p>: null}
+      
+      </div>
+    <div className="wind">
+      <p>Wind Speed</p>
+    {data.wind?<p className="bold">{data.wind.speed}MPH</p>:null }
+      
+      </div>
+    </div>
+  </div>
+</div>);
 }
 
 export default App;
