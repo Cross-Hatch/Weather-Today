058eb5b7ab58c37e4a0ca46f339ee295fd2b6a40
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-05-02 20:07:17 +0000

    Modified Header.js
    Added logo component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import Humburger from '../Humburger/Humburger';
+import Logo from '../Logo/Logo';
 import { Container } from './Header.style';
 
 const Header = () => {
   return (
     <Container>
+      <Logo />
       <Humburger />
     </Container>
   );
