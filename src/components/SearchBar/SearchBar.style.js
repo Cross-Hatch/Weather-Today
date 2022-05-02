87bfee9bf79fecac9e7ba0841f18fb50031e53c9
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-05-02 20:01:06 +0000

    Modified SearchBar.style.js
    Removed position absolute

--- src/components/SearchBar/SearchBar.style.js
+++ src/components/SearchBar/SearchBar.style.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  position: absolute;
-  bottom: 0vh;
+  /* position: absolute;
+  bottom: 0vh; */
   display: flex;
   justify-content: center;
   align-items: center;
