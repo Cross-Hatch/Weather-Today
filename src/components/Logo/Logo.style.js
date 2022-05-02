581d8829d028833dded47484d57f7a666906484d
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-05-02 20:05:36 +0000

    Added Logo.style.js

---
+++ src/components/Logo/Logo.style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Img = styled.img`
+  width: 2em;
+  margin: 0 0.5em 0 0;
+`;
+
+export const LogoName = styled.div`
+  font-weight: bolder;
+
+  span {
+    font-weight: 300;
+  }
+`;
