d5d2297e4a4a5dc3f4900054678776f452b381c2
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-05-06 05:14:40 +0000

    Modified App.js
    - Moved components to Layout.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import { ThemeProvider } from 'styled-components';
-import City from './components/City/City';
 import { GlobalStyle, Wrapper } from './components/GlobalStyle.style';
-import Header from './components/Header/Header';
-import SearchBar from './components/SearchBar/SearchBar';
 import { theme } from './components/ThemeProvider.style';
-import WeatherIcon from './components/WeatherIcon/WeatherIcon';
-import sunny from './assets/weather-icon/sunny.png';
-import CurrentTemprature from './components/CurrentTemprature/CurrentTemprature';
-import Greatings from './components/Greatings/Greatings';
-import AsidePanel from './components/AsidePanel/AsidePanel';
+import { WeatherContexProvider } from './hooks/WeatherContext';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Header />
-      <Wrapper>
-        <div>
-          <City city="Dansoman" timezone="Africa/Ghana" />
-          <WeatherIcon icon={sunny} alt="sunny" />
-          <CurrentTemprature
-            temprature={20}
-            unit={'°C'}
-            description={'Feels like 18°C'}
-          />
-
-          <Greatings time={'good morning'} day={'Sunday'} />
-          <SearchBar />
-        </div>
-      </Wrapper>
-      <AsidePanel icon={sunny} />
-    </ThemeProvider>
+    <WeatherContexProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Layout/>
+      </ThemeProvider>
+    </WeatherContexProvider>
   );
 }
 
