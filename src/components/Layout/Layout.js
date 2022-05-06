465ff203da9d0b175bb24cc596e442cee9f04e0f
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-05-06 05:14:02 +0000

    Modified Layout.js
    - Moved components from App.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,36 @@
+import City from '../City/City';
 import Header from '../Header/Header';
-import { Container, Wrapper } from './Layout.style';
+import SearchBar from '../SearchBar/SearchBar';
+import WeatherIcon from '../WeatherIcon/WeatherIcon';
+import sunny from '../../assets/weather-icon/sunny.png';
+import CurrentTemprature from '../CurrentTemprature/CurrentTemprature';
+import Greatings from '../Greatings/Greatings';
+import AsidePanel from '../AsidePanel/AsidePanel';
+import { Wrapper } from '../GlobalStyle.style';
+import {WeatherContext} from '../../hooks/WeatherContext';
+import { useContext } from 'react';
 
-const Layout = ({ children }) => {
+const Layout = () => {
+  const {data} = useContext(WeatherContext)
+
   return (
     <>
       <Header />
-      <Wrapper>
-        <Container>{children}</Container>
-      </Wrapper>
+        <Wrapper>
+          <div>
+            <City city={data.name} timezone="Africa/Ghana" />
+            <WeatherIcon icon={sunny} alt="sunny" />
+            <CurrentTemprature
+              temprature={20}
+              unit={'°C'}
+              description={'Feels like 18°C'}
+            />
+
+            <Greatings time={'good morning'} day={'Sunday'} />
+            <SearchBar />
+          </div>
+        </Wrapper>
+        <AsidePanel icon={sunny} />
     </>
   );
 };
