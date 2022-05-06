52a5c447975a75a4ca2b11adde1707149f4d80e8
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-05-06 05:15:47 +0000

    Modified SearchBar.js
    - Added functionality to set search parameters

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,32 @@
 import { Container, SearchBox, SearchButton } from './SearchBar.style';
 import search from '../../assets/search-button.svg';
+import useFetchWeather from '../../hooks/useFetchWeather';
+import { useContext, useState } from 'react';
+import { WeatherContext } from '../../hooks/WeatherContext';
 
 const SearchBar = () => {
+  const [temporary, setTemporary] = useState();
+  const {setValue} = useContext(WeatherContext)
+
+  function searchLocation(e) {
+    if (e.key === 'Enter') {
+      setValue(temporary);
+    }
+  }
+  // console.log(data);
+
   return (
-    <Container>
-      <SearchBox type="text" placeholder="Search" />
-      <SearchButton>
-        <img src={search} alt="Search button" />
-      </SearchButton>
-    </Container>
+      <Container>
+        <SearchBox
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setTemporary(e.target.value)}
+          onKeyUp={searchLocation}
+        />
+        <SearchButton>
+          <img src={search} alt="Search button" />
+        </SearchButton>
+      </Container>
   );
 };
 
