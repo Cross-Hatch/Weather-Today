0334c4b502e4054496575473433ab9ccc1935f1e
Schniffenhausen <56304597+Kuameh@users.noreply.github.com>
2022-05-14 16:22:39 +0000

    Modified Layout.js
    - Added getDay function
    - Added getGreet function
    - Modified useContext

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -11,26 +11,50 @@ import {WeatherContext} from '../../hooks/WeatherContext';
 import { useContext } from 'react';
 
 const Layout = () => {
-  const {data} = useContext(WeatherContext)
+  const {data, error, location} = useContext(WeatherContext)
+  const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+  console.log(location);
+
+  function getDay(e) {
+    const dd = new Date(e * 1000);
+    return days[dd.getDay()];
+  }
+
+  function getGreet(e) {
+    const dd = new Date(e * 1000);
+    let greet
+
+    if (dd.getHours() < '12') {
+      greet = 'good morning'
+    }else if (dd.getHours() < '17') {
+      greet = 'good afternoon'
+    }else if (dd.getHours() >= '17') {
+      greet = 'good evening'
+    }else {
+      greet = 'error'
+    }
+    console.log(dd.getHours());
+    return greet
+  }
 
   return (
     <>
       <Header />
         <Wrapper>
           <div>
-            <City city={data.name} timezone="Africa/Ghana" />
+            <City city={location} timezone={data.timezone} />
             <WeatherIcon icon={sunny} alt="sunny" />
             <CurrentTemprature
-              temprature={20}
+              temprature={Math.floor(data.current.temp)}
               unit={'°C'}
-              description={'Feels like 18°C'}
+              description={`Feels like ${Math.floor(data.current.feels_like)}°C`}
             />
 
-            <Greatings time={'good morning'} day={'Sunday'} />
+            <Greatings time={getGreet(data.current.dt)} day={getDay(data.current.dt)} />
             <SearchBar />
           </div>
         </Wrapper>
-        <AsidePanel icon={sunny} />
+        <AsidePanel />
     </>
   );
 };
